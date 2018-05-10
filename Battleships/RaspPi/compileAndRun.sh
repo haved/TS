4264ee9417e6f4968873508c6184ab79809b77d5
haved <krogstie.havard@gmail.com>
2018-05-10 21:56:47 +0200

    Fix paths in scripts for the Pi

--- Battleships/RaspPi/compileAndRun.sh
+++ Battleships/RaspPi/compileAndRun.sh
@@ -2,5 +2,4 @@ mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build .
-cd ..
-./build/Battleships #&> ./build/log.txt
+./Battleships #&> ./log.txt
--- Battleships/RaspPi/debug.sh
+++ Battleships/RaspPi/debug.sh
@@ -2,5 +2,4 @@ mkdir -p debugBuild
 cd debugBuild
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DDEBUG_IO=YES &&
 cmake --build . &&
-cd ..
 ./Battleships
