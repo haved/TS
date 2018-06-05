62aba929131048cc6817b6b0f7015737a4b892d9
haved <krogstie.havard@gmail.com>
2018-06-05 10:45:34 +0200

    Start doing drawing maybe, just a properly sized canvas for now

--- Battleships/PureArduinoSim/main.cpp
+++ Battleships/PureArduinoSim/main.cpp
@@ -21,9 +21,15 @@ void fillScreen(int screen, CRGB color) {
 void startTransition(int screen, int frames) {
 
 }
-void updateScreens() {
 
+#define TILE_SIZE 20
+#define SCREEN_MARGIN 20
+cairo_surface_t* surface;
+cairo_t* cr;
+void updateScreens() {
+	
 }
+
 bool anyTransitionRunning() {
 	return transGoingMask != 0;
 }
@@ -46,7 +52,7 @@ int getKeyIndex(int keyval) {
 gboolean on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user_data) {
 	int keyIndex = getKeyIndex(event->keyval);
 	if(keyIndex < 0)
-		return false;
+		return false; //We didn't handle the key event
 	currentButtonState.raw[keyIndex] = true;
 	return true;
 }
@@ -54,7 +60,7 @@ gboolean on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user_data)
 gboolean on_key_release(GtkWidget *widget, GdkEventKey *event, gpointer user_data) {
 	int keyIndex = getKeyIndex(event->keyval);
 	if(keyIndex < 0)
-		return false;
+		return false; //We didn't handle the key event
 	currentButtonState.raw[keyIndex] = false;
 	return true;
 }
@@ -62,12 +68,19 @@ gboolean on_key_release(GtkWidget *widget, GdkEventKey *event, gpointer user_dat
 void loop(); //In GameLogic.cpp
 gboolean main_tick(GtkWidget* widget, GdkFrameClock* frame_clock, gpointer user_data) {
 	loop();
-	return true;
+	return true; //Keep going next frame
 }
 
 int main(int argc, char** argv) {
 	gtk_init(&argc, &argv);
 	GtkWidget* window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), "PureArduinoSim");
+	gtk_container_set_border_width (GTK_CONTAINER (window), 8);
+
+	GtkWidget* drawing_area = gtk_drawing_area_new();
+	gtk_widget_set_size_request(drawing_area, TILE_SIZE*WIDTH, TILE_SIZE*HEIGHT*4+SCREEN_MARGIN*3);
+	gtk_container_add(GTK_CONTAINER(window), drawing_area);
+
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
 	gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
@@ -77,7 +90,15 @@ int main(int argc, char** argv) {
 
 	gtk_widget_add_tick_callback(window, main_tick, NULL, NULL);
 
-	gtk_widget_show(window);
+	gtk_widget_show_all(window);
+
+	surface = gdk_window_create_similar_surface (gtk_widget_get_window (drawing_area),
+												 CAIRO_CONTENT_COLOR,
+												 gtk_widget_get_allocated_width (drawing_area),
+												 gtk_widget_get_allocated_height (drawing_area));
+	cr = cairo_create(surface);
+
 	gtk_main();
+	cairo_destroy(cr);
 	return 0;
 }
