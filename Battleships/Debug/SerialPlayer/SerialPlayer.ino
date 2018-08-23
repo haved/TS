d9d98b7d0b7241a9b3d12b56f89cd4f77f13e4e1
haved <krogstie.havard@gmail.com>
2018-08-23 23:15:37 +0200

    It works! Yaaay

--- Battleships/Debug/SerialPlayer/SerialPlayer.ino
+++ Battleships/Debug/SerialPlayer/SerialPlayer.ino
@@ -1,13 +1,13 @@
 void setup() {
   Serial.begin(115200);
   Serial.print("XXX");
+  
+  Serial.print(">M");
+  Serial.write(1);
 }
 
 void loop() {
   delay(4000);
-  
-  Serial.print(">M");
-  Serial.write(1);
   
   Serial.print(">s");
   Serial.write(2);
