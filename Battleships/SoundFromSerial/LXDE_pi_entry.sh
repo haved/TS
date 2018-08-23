5028c9d96f82931aece573a84387315870d47c19
haved <krogstie.havard@gmail.com>
2018-08-23 21:26:10 +0200

    Add some sleep before listening

--- Battleships/SoundFromSerial/LXDE_pi_entry.sh
+++ Battleships/SoundFromSerial/LXDE_pi_entry.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Append following line to .config/lxsession/LXDE-pi/autostart (without the # symbol)
 #lxterminal -l -e /home/pi/Development/TS/Battleships/SoundFromSerial/LXDE_pi_entry.sh
+sleep 4
 cd /home/pi/Development/TS/Battleships/SoundFromSerial/
 bash ./auto_listener.sh
